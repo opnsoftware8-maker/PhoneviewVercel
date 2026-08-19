bf921d25f0be98cbe083c1078c2696df53f2dfc0
opnsoftware8-maker <opnsoftware8@gmail.com>
2026-08-19 14:46:01 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -142,7 +142,7 @@ export default function App() {
         </div>
         
         {/* กล่องแสดงผลลัพธ์ Card มือถือ */}
-        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
           {PhoneArr.length > 0 ? (
             PhoneArr.map((Data, index) => (
               <Phonecard key={Data.id || index} Device={Data} />

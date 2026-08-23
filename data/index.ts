11b95542b0b59462cd1393a7bdfecb5d3e139955
opnsoftware8-maker <opnsoftware8@gmail.com>
2026-08-23 09:09:58 +0700

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -1,6 +1,14 @@
 import { xiaomiphone } from "./Phone/xioami"
+import { samsungphone } from "./Phone/samsung"
+import { vivophone } from "./Phone/vivo"
+import { applephone } from "./Phone/apple"
+import { oppophone } from "./Phone/oppo"
 
 export const Allphone = [
   ...xiaomiphone,
+  ...vivophone,
+  ...oppophone,
+  ...samsungphone,
+  ...applephone,
 
 ];

f07598659007e4e948f8240252a712d785a0c658
opnsoftware8-maker <opnsoftware8@gmail.com>
2026-09-14 09:45:02 +0700

    feat: add realme brand and realme GT 7T to data
    Include realme phone data in the Allphone collection and export it for use across the application.

--- data/index.ts
+++ data/index.ts
@@ -5,13 +5,15 @@ import { applephone } from "./Phone/apple"
 import { oppophone } from "./Phone/oppo"
 import { huaweiphone } from "./Phone/huawei"
 import { infinixphone } from "./Phone/infinix"
+import { realmephone } from "./Phone/realme"
 
-export { xiaomiphone, samsungphone, vivophone, applephone, oppophone, huaweiphone, infinixphone };
+export { xiaomiphone, samsungphone, vivophone, applephone, oppophone, huaweiphone, infinixphone, realmephone };
 
 export const Allphone = [
   ...xiaomiphone,
   ...vivophone,
   ...oppophone,
+  ...realmephone,
   ...infinixphone,
   ...samsungphone,
   ...applephone,

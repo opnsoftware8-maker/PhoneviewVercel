f2cecf20844bbc0909ded4887f0579616dcb6c34
opnsoftware8-maker <opnsoftware8@gmail.com>
2026-09-14 09:05:01 +0700

    feat: add OPPO Find X9 Ultra and Infinix GT 50 Pro
    Update device data with new models and integrate Infinix into the global phone registry.

--- data/index.ts
+++ data/index.ts
@@ -4,14 +4,16 @@ import { vivophone } from "./Phone/vivo"
 import { applephone } from "./Phone/apple"
 import { oppophone } from "./Phone/oppo"
 import { huaweiphone } from "./Phone/huawei"
+import { infinixphone } from "./Phone/infinix"
 
+export { xiaomiphone, samsungphone, vivophone, applephone, oppophone, huaweiphone, infinixphone };
 
 export const Allphone = [
   ...xiaomiphone,
   ...vivophone,
   ...oppophone,
+  ...infinixphone,
   ...samsungphone,
   ...applephone,
   ...huaweiphone,
-
 ];
